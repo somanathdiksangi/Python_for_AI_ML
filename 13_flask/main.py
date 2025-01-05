36875e63df51a73a51932e208a41162186e9215a
somanathdiksangi <somanathdiksangi@gmail.com>
2025-01-05 15:38:42 +0530

    update

---
+++ 13_flask/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route("/")
+
+
+def home_page():
+    return "this is my home page"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+    
